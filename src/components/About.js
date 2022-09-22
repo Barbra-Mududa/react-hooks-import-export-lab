1298b7592c582f25d97bac60c56705ffd9f42e7c
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-09-22 19:43:06 +0300

    Completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function About() {
+function About(user) {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={user.image} alt="I made this" />
     </div>
   );
 }
+
+export default About
